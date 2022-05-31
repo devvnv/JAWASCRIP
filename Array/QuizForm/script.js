794c6ddb28fd7bf7f0da1d3d7426cdc8dc448bc5
devvnv <devangvanvi16@gmail.com>
2022-05-31 20:28:33 +0530

    helo

---
+++ Array/QuizForm/script.js
@@ -0,0 +1,101 @@
+const quizData = [
+  {
+    question: "Which language runs in a web browser?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "What does CSS stand for?",
+    a: "Central Style Sheets",
+    b: "Cascading Style Sheets",
+    c: "Cascading Simple Sheets",
+    d: "Cars SUVs Sailboats",
+    correct: "b",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Hypertext Markdown Language",
+    c: "Hyperloop Machine Language",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+  },
+];
+
+// const quiz = document.getElementById("quiz");
+// const answerElements = document.querySelectorAll(".answer");
+// const questionElement = document.getElementById("question");
+// const a_text = document.getElementById("a_text");
+// const b_text = document.getElementById("b_text");
+// const c_text = document.getElementById("c_text");
+// const d_text = document.getElementById("d_text");
+// const submitButton = document.getElementById("submit");
+
+// let currentQuiz = 0;
+// let score = 0;
+
+// function loadQuiz()
+// {
+  
+// }
+
+
+// loadQuiz();
+
+// var html = "";
+
+// for(let i=0; i< quizData.length;){
+//  html = html + `<div class="quiz-container" id="quiz">
+//  <div class="quiz-header">
+//    <h2 id="question">${quizData[i].question}</h2>
+//    <ul>
+//      <li>
+//        <input type="radio" name="answer" id="a" class="answer" />
+//        <label for="a" id="a_text">${quizData[i].a}</label>
+//      </li>
+//      <li>
+//        <input type="radio" name="answer" id="b" class="answer" />
+//        <label for="b" id="b_text">${quizData[i].b}</label>
+//      </li>
+//      <li>
+//        <input type="radio" name="answer" id="c" class="answer" />
+//        <label for="c" id="c_text">${quizData[i].c}</label>
+//      </li>
+//      <li>
+//        <input type="radio" name="answer" id="d" class="answer" />
+//        <label for="d" id="d_text">${quizData[i].d}.</label>
+//      </li>
+//    </ul>
+//  </div>
+//  <button id="submit">Submit</button>
+//  </div>`
+// }
+
+// document.getElementById("quiz").innerHTML = html;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
